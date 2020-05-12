54d97e4e8433ff1491fac9a04fc0a3de930044b9
Munekatsu Furugen <rinconlincon@gmail.com>
2020-05-12 15:13:44 +0900

    Implemented update and destroy actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,44 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    params[:task][:completed] == '1' ? params[:task][:completed] = true : params[:task][:completed] = false
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
